4981b6f279fdd8322d72b16ef420a63d2fec44cd
Itihell Mejia <itihell.mejia@gmail.com>
2025-10-15 18:47:10 -0600

    Plantilla del card desde javascript

--- static/js/fetching.js
+++ static/js/fetching.js
@@ -1,9 +1,40 @@
-(() => {
+(async () => {
+  const containerItems = document.getElementById("list-card");
+  const createCard = (rows) => {
+    rows.forEach((element) => {
+      const card = `<div class="content-cards">
+        <div class="content-img">
+          <img src="static/img/${element.img}" alt="${element.nombre}" />
+        </div>
+        <div class="content-info">
+          <div class="margin-10">
+            <h4>Dato del curso</h4>
+            <hr />
+            <div class="content-text">
+              <div>Código:</div>
+              <div>${element.codigo}</div>
+            </div>
+            <div class="content-text">
+              <div>Nombre:</div>
+              <div>${element.nombre}</div>
+            </div>
+            <div class="content-text">
+              <div>Créditos:</div>
+              <div>${element.creditos}</div>
+            </div>
+          </div>
+        </div>
+      </div>`;
+      containerItems.innerHTML += card;
+      console.log(element);
+    });
+  };
   async function getJson() {
     const url = `/static/data/cursos.json`;
     const rowsRaw = await fetch(url);
     const rows = await rowsRaw.json();
-    console.log({ rows });
+    return rows;
   }
-  getJson();
+  const rows = await getJson();
+  createCard(rows);
 })();
