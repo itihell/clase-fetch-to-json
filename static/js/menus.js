3b5c653da8f5fd2f00947a67a54afdf8e13c8bc2
Itihell Mejia <itihell.mejia@gmail.com>
2025-10-15 20:14:33 -0600

    Renderizado de plantilla del menus con javascript

--- static/js/menus.js
+++ static/js/menus.js
@@ -1,3 +1,19 @@
 (async () => {
-    
+  const containerMenus = document.getElementById("nav-list");
+  const getMenus = async () => {
+    const url = "/static/data/menus.json";
+    const response = await fetch(url);
+    const menus = await response.json();
+    return menus;
+  };
+
+  const createMenus = (menus) => {
+    menus.forEach((element) => {
+      const plantilla = `<li><a href="${element.link}">${element.name}</a></li>`;
+      containerMenus.innerHTML += plantilla;
+    });
+  };
+
+  const menus = await getMenus();
+  createMenus(menus);
 })();
